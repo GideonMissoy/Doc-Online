f082099c1672520ecce003d7bb446133ba6f4914
GideonMissoy <giddykips2030@gmail.com>
2024-08-24 20:02:08 +0300

    Setup and handling user accounts

---
+++ Backend/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+# from api.views import CreateUserView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/users/auth/', include('Accounts.urls')),
+    # path("api/", include("api.urls")),
+]
